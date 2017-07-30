ba233ce7edc3d4b979983f7aed42e77d5a707245
Andrew Chang <achang232@comcast.net>
2017-07-30 18:18:35 -0500

    adding fundamentals folder for Coding Dojo.

---
+++ CodingDojo_RoR/Fundamentals/ruby_tests/excercise1.rb
@@ -0,0 +1,25 @@
+# puts "hello"
+# puts "Andrew"
+# puts "rails"
+
+# x = "CodingDojo"
+# puts x
+# puts x.class
+# puts x.include?("Dojo")
+# puts "this sting includes 'Dojo'" if x.include?("Dojo")
+
+n = "Andrew, Kevin, Chang"
+puts n.split(",")
+puts "hello"
+puts "CodingDojo"
+
+BEGIN {
+   puts "this is the being block"
+}
+
+END {
+   puts "this is the end"
+}
+
+z =100
+puts "the value of z is #{z}"
