84ff809ce243a968edb156b650cfb1ac8f0dcad6
Andrew Chang <achang232@comcast.net>
2017-09-22 15:24:39 -0500

    fix routing issue with POST route to create new posts in layout page.

--- CodingDojo_RoR/Rails_vagrant_box/Rails/Controllers_and_Views/Layout_Assignment/app/controllers/posts_controller.rb
+++ CodingDojo_RoR/Rails_vagrant_box/Rails/Controllers_and_Views/Layout_Assignment/app/controllers/posts_controller.rb
@@ -5,11 +5,10 @@ class PostsController < ApplicationController
      render layout: 'three_column'
   end
   def create
-     #post data is not working correctly need to fix. Says it is expected but receiving string data instead. User(#70331409493480) expected, got String(#9642080) some type of mismatch
 
-     @posts = Post.create(title:params[:title], content:params[:content], user:User.find(params[:user])
+     @posts = Post.create(title:params[:title], content:params[:content], user:User.find(params[:user]))
      if @posts.save
-        redirect_to 'posts'
+        redirect_to posts_path
      else
         flash[:errors] = posts.errors.full_messages
         redirect_to :back
